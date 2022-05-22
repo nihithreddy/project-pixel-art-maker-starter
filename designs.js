1a6bb55f73990187eccaef8c7feae8869cfcea98
Nihith <sainihith9618@gmail.com>
2022-05-22 18:07:32 +0530

    added code for grid creation

--- designs.js
+++ designs.js
@@ -2,9 +2,31 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const colorInput = document.querySelector("#colorPicker")
 
 function makeGrid() {
-
-// Your code goes here!
-
+    // Your code goes here!
+    const tableElement =  document.querySelector("#pixelCanvas")
+    const prevTableBody = document.getElementsByTagName("tbody")
+    if(prevTableBody.length !== 0){
+        tableElement.removeChild(prevTableBody[0])
+    }
+    const rows = parseInt(document.getElementById("inputHeight").value)
+    const columns = parseInt(document.getElementById("inputWidth").value)
+    console.log(rows,columns)
+    const tableBody = document.createElement("tbody")
+    for(let row = 0; row < rows; row ++){
+        const tableRow = document.createElement("tr")
+        for(let col = 0; col < columns ; col++){
+            const tableCell = document.createElement("td")
+            tableCell.addEventListener("click",function(event){
+                event.target.style.backgroundColor = colorInput.value
+            })
+            tableRow.appendChild(tableCell)
+        }
+        tableBody.appendChild(tableRow)
+    }
+    tableElement.appendChild(tableBody)
 }
+
+
