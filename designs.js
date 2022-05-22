67d0147687f4a6d878b6aeb8f2492e3f67041ac9
Nihith <sainihith9618@gmail.com>
2022-05-22 18:12:49 +0530

    commented the code

--- designs.js
+++ designs.js
@@ -1,25 +1,25 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 const colorInput = document.querySelector("#colorPicker")
 
 function makeGrid() {
     // Your code goes here!
     const tableElement =  document.querySelector("#pixelCanvas")
     const prevTableBody = document.getElementsByTagName("tbody")
+    //Clear the previous grid
     if(prevTableBody.length !== 0){
         tableElement.removeChild(prevTableBody[0])
     }
+    //Parsing the rows (height) and columns (width) values
     const rows = parseInt(document.getElementById("inputHeight").value)
     const columns = parseInt(document.getElementById("inputWidth").value)
-    console.log(rows,columns)
     const tableBody = document.createElement("tbody")
+    //Dynamically creating the canvas
     for(let row = 0; row < rows; row ++){
         const tableRow = document.createElement("tr")
         for(let col = 0; col < columns ; col++){
             const tableCell = document.createElement("td")
             tableCell.addEventListener("click",function(event){
+                //Dynamically setting the backgroundcolor to the table cell
                 event.target.style.backgroundColor = colorInput.value
             })
             tableRow.appendChild(tableCell)
